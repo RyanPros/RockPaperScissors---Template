330eaca3845b57d1520e64cc80c4a1fd66ef5bca
Ryan Prosper <ryanpros178@outlook.com>
2021-02-25 14:36:55 -0500

    Hi

--- RockPaperScissors/Form1.cs
+++ RockPaperScissors/Form1.cs
@@ -48,16 +48,143 @@ namespace RockPaperScissors
         {
             /// TODO Set the playerchoice value, show the appropriate image,
             /// play a sound, wait for a second; repeat for the computer turn 
+            ComputerChoice();
+            playerChoice = "rock";
+            playerImage.BackgroundImage = rockImage; //Properties.Resources.rock168x280;
+            jabPlayer.Play();
+            Refresh();
+            Thread.Sleep(choicePause);
+
+            if (playerChoice == cpuChoice)
+            {
+                resultImage.BackgroundImage = tieImage;
+                ties++;
+                tiesLabel.Text = $"Ties: {ties}";
+            }
+            else if (cpuChoice == "scissors")
+            {
+                resultImage.BackgroundImage = winImage;
+                wins++;
+                winsLabel.Text = $"Wins: {wins}";
+
+            }
+            else 
+            {
+                resultImage.BackgroundImage = loseImage;
+                losses++;
+                lossesLabel.Text = $"Loss: {losses}";
+
+            }
+            gongPlayer.Play();
+            Refresh();
+            Thread.Sleep(outcomePause);
+
+            playerImage.BackgroundImage = null;
+            cpuImage.BackgroundImage= null;
+            resultImage.BackgroundImage = null;
         }
 
         private void paperButton_Click(object sender, EventArgs e)
         {
+            ComputerChoice();
+            playerChoice = "paper";
+            playerImage.BackgroundImage = paperImage; //Properties.Resources.rock168x280;
+            {
+                cpuChoice = "scissors";
+                cpuImage.BackgroundImage = scissorImage;
+            }
 
+            jabPlayer.Play();
+            Refresh();
+            Thread.Sleep(choicePause);
+
+            if (playerChoice == cpuChoice)
+            {
+                resultImage.BackgroundImage = tieImage;
+                ties++;
+                tiesLabel.Text = $"Ties: {ties}";
+            }
+            else if (cpuChoice == "rock")
+            {
+                resultImage.BackgroundImage = winImage;
+                wins++;
+                winsLabel.Text = $"Wins: {wins}";
+
+            }
+            else
+            {
+                resultImage.BackgroundImage = loseImage;
+                losses++;
+                lossesLabel.Text = $"Loss: {losses}";
+
+            }
+            gongPlayer.Play();
+            Refresh();
+            Thread.Sleep(outcomePause);
+
+            playerImage.BackgroundImage = null;
+            cpuImage.BackgroundImage = null;
+            resultImage.BackgroundImage = null;
         }
 
         private void scissorsButton_Click(object sender, EventArgs e)
         {
+            playerChoice = "scissors";
+            playerImage.BackgroundImage = scissorImage; //Properties.Resources.rock168x280;
 
+            ComputerChoice();
+
+            jabPlayer.Play();
+            Refresh();
+            Thread.Sleep(choicePause);
+
+            if (playerChoice == cpuChoice)
+            {
+                resultImage.BackgroundImage = tieImage;
+                ties++;
+                tiesLabel.Text = $"Ties: {ties}";
+            }
+            else if (cpuChoice == "paper")
+            {
+                resultImage.BackgroundImage = winImage;
+                wins++;
+                winsLabel.Text = $"Wins: {wins}";
+
+            }
+            else
+            {
+                resultImage.BackgroundImage = loseImage;
+                losses++;
+                lossesLabel.Text = $"Loss: {losses}";
+
+            }
+            gongPlayer.Play();
+            Refresh();
+            Thread.Sleep(outcomePause);
+
+            playerImage.BackgroundImage = null;
+            cpuImage.BackgroundImage = null;
+            resultImage.BackgroundImage = null;
+        }
+        public void ComputerChoice()
+        {
+            int value = randGen.Next(1, 4);
+            if (value == 1)
+            {
+                cpuChoice = "rock";
+                cpuImage.BackgroundImage = rockImage;
+            }
+            else if (value == 2)
+            {
+                cpuChoice = "paper";
+                cpuImage.BackgroundImage = paperImage;
+            }
+            else if (value == 3)
+            {
+                cpuChoice = "scissors";
+                cpuImage.BackgroundImage = scissorImage;
+            }
         }
     }
+
 }
